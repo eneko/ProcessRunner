6a708ec2dc1e30f753cda812cd70394c3b92a4d8
Eneko Alonso <eneko.alonso@gmail.com>
2018-08-28 20:16:57 -0700

    Fix standard error test expectation for Linux

--- Tests/SystemTests/StandardErrorTests.swift
+++ Tests/SystemTests/StandardErrorTests.swift
@@ -3,12 +3,20 @@ import System
 
 final class StandardErrorTests: XCTestCase {
 
+    var expectedError: String {
+      #if os(Linux)
+        return "cat: unrecognized option"
+      #else
+        return "cat: illegal option"
+      #endif
+    }
+
     func testStandardError() throws {
-        XCTAssertTrue(try system(command: "cat --foobar", captureOutput: true).standardError.contains("illegal option"))
+        XCTAssertTrue(try system(command: "cat --foobar", captureOutput: true).standardError.contains(expectedError))
     }
 
     func testStandardErrorShell() throws {
-        XCTAssertTrue(try system(shell: "cat --foobar", captureOutput: true).standardError.contains("illegal option"))
+        XCTAssertTrue(try system(shell: "cat --foobar", captureOutput: true).standardError.contains(expectedError))
     }
 
     static var allTests = [
